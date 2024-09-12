0add2d246c9d175361a8cc383847866a4d7fb990
Caden <caden.chock@menloschool.org>
2024-09-12 15:13:25 -0700

    :/

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -17,9 +17,12 @@ public class HighwaysAndHospitals {
      *  hospital access for all citizens in Menlo County.
      */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+        long num = n;
+        long road = highwayCost;
+        long hospital = hospitalCost;
         // If Hospital Cheaper than Highway Better to Just Build Only Hospital for Each City
         if(hospitalCost < highwayCost) {
-            return n*hospitalCost;
+            return num*hospital;
         }
 
         int[] group = new int[n+1];
@@ -30,29 +33,30 @@ public class HighwaysAndHospitals {
         for(int i = 0; i < cities.length; i++){
             int city2 = cities[i][1];
             int city1 = cities[i][0];
-            if(group[city2] == 0){
+            if(group[city2] == 0 && city2 != group[city1]){
                 group[city2] = city1;
             }
             else{
-                group[city2] = group[city1];
+                if(group[city2] != group[city1]){
+                    group[group[city2]] = city1;
+                }
             }
-            /*
             for(int j = 1; j < group.length; j++){
                 if(group[j] == city2){
                     group[j] = city1;
                 }
             }
-             */
         }
-        int roots = 0;
+        long roots = 0;
         for(int i = 1; i < group.length; i++){
             if(group[i] == 0){
                 roots++;
             }
         }
-        long cost = 0;
+
         // Total = hospitalCost * #Cluster + highwayCost (Cities - # clusters)
-        cost = (hospitalCost * roots) + (highwayCost * (n - roots));
+        long cost = (hospital * roots);
+        cost += (road * (num - roots));
         return cost;
     }
 }
