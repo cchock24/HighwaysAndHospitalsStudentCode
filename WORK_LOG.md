14d010ac2420cb1a5a5511dbb3b0393f0c658e20
Caden <caden.chock@menloschool.org>
2024-09-12 00:11:13 -0700

    :/

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,11 +1,14 @@
 Name: YOUR NAME HERE
 
-| Date    |    Time     |                                                                                       Update |
-|:--------|:-----------:|---------------------------------------------------------------------------------------------:|
-| April 5 |   9-10pm    |                                                   I tried to write abstract, efficient code. |
-| Sep 9   |  9:50-9:55  |                                                                Read the Files and Test Files |
-| Sep 9   | 10:30:11:00 | Thought About the Problem. Quickly did the case when Hospital Cost Cheaper than Highway Cost |
-| Sep 10  |  2:00-2:30  |            A lot of thinking. make Code that counts number of connections each city can make |
+| Date    |    Time     |                                                                                                            Update |
+|:--------|:-----------:|------------------------------------------------------------------------------------------------------------------:|
+| April 5 |   9-10pm    |                                                                        I tried to write abstract, efficient code. |
+| Sep 9   |  9:50-9:55  |                                                                                     Read the Files and Test Files |
+| Sep 9   | 10:30:11:00 |                      Thought About the Problem. Quickly did the case when Hospital Cost Cheaper than Highway Cost |
+| Sep 10  |  2:00-2:30  |                                 A lot of thinking. make Code that counts number of connections each city can make |
+| Sep 10  | 10:00-10:30 | I was trying to make arrays of arrays/arraylists to help record the number of connections, but it wasn't working. |
+| Sep 11  | 11:00-11:30 |                                         I'm Trying to make a look that adds all things connecting to 1 to a group |
+| Sep 11  | 11:30-12:00 |                                  Trying to make a loop so it goes through all the cities and adds them to a group |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+// Highways and Hospitals by Caden Chock
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
@@ -21,21 +21,75 @@ public class HighwaysAndHospitals {
         if(hospitalCost < highwayCost) {
             return n*hospitalCost;
         }
-
+        // Declare Lists
+        ArrayList<Integer>[] connections = new ArrayList[n];
+        ArrayList<Integer>[] group = new ArrayList[n+1];
+        //Initialize Lists
+        for(int i = 0; i < n; i++){
+            connections[i] = new ArrayList<Integer>();
+            group[i] = new ArrayList<Integer>();
+        }
+        group[n] = new ArrayList<Integer>();
         // Find the Number of Connections of Each City
-        int[] numConnect = new int[n];
         for(int i = 0; i < cities.length; i++){
             int city1 = cities[i][0];
             int city2 = cities[i][1];
-            numConnect[city1]++;
-            numConnect[city2]++;
+            if(i == city1){
+                connections[i].add(city2);
+                connections[city2].add(city1);
+            }
         }
-        // If Only One Connection then Make Connection
+        for(int i = 1; i <= n; i++){
+            group[n].add(i);
+        }
+        int d = 1;
+        // Add Each City to a Group until every city has been added to a group
+        while(!group[n].isEmpty()){
+            // Add a Starting City to Group 1
+            group[d].add(group[n].remove(0));
+            if(group[n].isEmpty()){
+                break;
+            }
+            // If Group 1 Increases Run Again
+            int num = group[d].size();
+            int num2 = group[d].size();
+            do{
+                num = group[d].size();
+                // If Connections to 1 add to Group1 otherwise add to different group (Skip 1)
+                for(int i = 0; i < group[n].size(); i++){
+                    // Checks if City has any Connections
+                    if(!connections[group[n].get(i)].isEmpty()){
+                        for(int j = 0; j < connections[group[n].get(i)].size(); j++){
+                            // If Connected to Something in Group
+                            {
+                                for(int k = 0; k < group[d].size(); k++){
+                                    if(connections[group[n].get(i)].get(j) == group[d].get(k)){
+                                        group[d].add(i); // Talk To Mr. Blick About Effect
+                                        group[n].remove(i);
+                                    }
+                                }
+                            }
+                        }
+                    }
 
-
-        // Find Longest Single Connection Between Cities
-        //Take Spot see what connects to that spot see what connects to that spot
-
-        return 0;
+                }
+                num2 = group[1].size();
+                if(group[n].isEmpty()){
+                    break;
+                }
+            } while(num2 > num);
+            d++;
+        }
+        int cost = 0;
+        // Each Connection clump H + (v-1)R
+        for(int i = 0; i < group.length-1; i++){
+            if(!group[i].isEmpty()){
+                cost += hospitalCost + (group[i].size() -1) * highwayCost;
+            }
+        }
+        return cost;
     }
 }
+/*
+
+*/
